37de15ddde088352c736fcb700030ab2e3441872
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-05-30 21:13:54 -0300

    rabbit-mq send and consumer working

--- src/modules/rabitt-mq/rabitt-mq.service.ts
+++ src/modules/rabitt-mq/rabitt-mq.service.ts
@@ -5,18 +5,30 @@ import * as rabbitmq from 'amqplib';
 export class RabittMqService {
   private channel: any;
   constructor(@Inject('RABBIT_MQ_URL') private url: string) {
-    this.channel = rabbitmq
+    rabbitmq
       .connect(url)
-      .then(connection => connection.createChannel())
+      .then(connection =>
+        connection
+          .createChannel()
+          .then(channel => (this.channel = channel))
+          .catch(err => Logger.error('RabbitMQ channel error: ', err)),
+      )
       .error(err => Logger.error('RabbitMQ connection error: ', err));
   }
 
   send(queueName: string, message: any) {
+    this.channel.assertQueue(queueName);
     this.channel.sendToQueue(queueName, Buffer.from(message));
     return true;
   }
 
   async consumer(queueName: string) {
-    return this.channel.consume(queueName);
+    let mensagem = '';
+    this.channel.assertQueue(queueName);
+    await this.channel.consume(queueName, function(msg) {
+      console.log(' [x] Received %s', msg.content.toString());
+      mensagem = msg.content.toString();
+    });
+    return mensagem;
   }
 }
